555b05a114b5e90344e47c563b5e3071f1fb8925
림림 <hyerim23@ewhain.net>
2025-12-02 21:46:00 +0900

    fix: alert창 작동

--- app.py
+++ app.py
@@ -249,7 +249,7 @@ def view_review():
         page_count=page_count,
         total=total,         # 검색 결과 개수
         total_all=total_all, # 전체 리뷰 개수 (필요하면 상단에 사용)
-        sort=sort,           # 🔥 템플릿에서 지금 정렬상태 알 수 있게 넘겨줌
+        sort=sort,           # 템플릿에서 지금 정렬상태 알 수 있게 넘겨줌
         q=q,                 # 검색창에 기존 검색어 유지용
     )
 
@@ -264,6 +264,11 @@ def review_detail(review_id):
 
 @application.route("/reg_items")
 def reg_item():
+    # 로그인 안한 상태로 상품 등록 불가
+    if not session.get('id'):
+        flash("로그인 후 상품을 등록할 수 있습니다.")
+        return redirect(url_for('view_login'))
+    
     return render_template("reg_items.html")
 
 @application.route("/reg_reviews")
@@ -277,6 +282,12 @@ def reg_reviews_legacy():
 @application.route("/reg_review_init/<name>/")
 def reg_review_init(name):
     # name: 상품 이름 (상품 상세에서 넘어올 때만 채워짐)
+    
+    # 로그인 안한 상태로 리뷰 등록 불가
+    if not session.get('id'):
+        flash("로그인 후 리뷰을 등록할 수 있습니다.")
+        return redirect(url_for('view_login'))
+    
     return render_template("reg_reviews.html", name=name)
 
 
